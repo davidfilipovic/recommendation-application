c3adb3af07c0e7852f85f8af9d29d445c5a5b671
David <david.1990@ymail.com>
2014-06-30 13:33:28 +0200

    Some functionalities added

---
+++ recommendation-application/bin/recommendation_application/core_test.clj
@@ -0,0 +1,7 @@
+(ns recommendation-application.core-test
+  (:require [clojure.test :refer :all]
+            [recommendation-application.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
--- recommendation-application/project.clj
+++ recommendation-application/project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring "1.3.0"]
+                 [congomongo "0.4.4"]
+                 [compojure "1.1.8"]
+                 [hiccup "1.0.5"]
+                 [lib-noir "0.8.4"]]
+  :main recommendation-application.core)
