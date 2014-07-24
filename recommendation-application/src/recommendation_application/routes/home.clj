7d6034957f26cae8d9c1944dfd9f6fd9b83fbdd1
David <david.1990@ymail.com>
2014-07-24 04:41:19 +0200

    Added front left and right col

--- recommendation-application/src/recommendation_application/routes/home.clj
+++ recommendation-application/src/recommendation_application/routes/home.clj
@@ -11,22 +11,23 @@
  
 
 
+ 
 (defn layout 
-  [& body]  
+  [left-content & right-content]  
     (html5   
       [:head
        [:meta {:charset "utf=8"}]
       (include-css "/css/main.css")
        (include-css "/css/login.css")
        (include-css "/js/prettyPhoto/css/prettyPhoto.css")
-       (include-js "/js/prettyPhoto/js/jquery.prettyPhoto.js")
+       (include-js "/js/jquery.prettyPhoto.js")
       (include-js "/js/jquery.cycle.all.js")
-      (include-js "/js/jquery_1.4.2.js")
+     (include-js "/js/jquery_1.4.2.js")
       (include-js "/js/easing.js")
-      (include-js "/js/tooltip/jquery.tools.min.js")
+     (include-js "/js/tooltip/jquery.tools.min.js")
       (include-js "/js/jqueryui.js")
       (include-js "/js/filterable.pack.js")
-      (include-js "/js/jquery.tabs/jquery.tabs.pack.js")
+     (include-js "/js/jquery.tabs/jquery.tabs.pack.js")
       (include-js "/js/custom.js")]
     (let [user (session/flash-get :username)]
       [:body
@@ -37,9 +38,7 @@
            [:div.title "AQA"]
            [:div.subtitle "kk"]
            [:div.username (str "Wellcome " user)
-             (link-to "/logout" "logout")]
-          ]]]]
-       
+             (link-to "/logout" "logout")]]]]]      
       [:div#main
        [:div.wrapper
         [:div.home-content
@@ -50,31 +49,71 @@
            [:span]]
           [:ul#slides
            [:li ;{:style "position: absolute; top: 0px; left: -960px; display: none; z-index: 3; opacity: 1; width: 960px; height: 323px;"} 
-            [:img {:src "images/1.jpg"}]]
+         [:img {:src "images/1.jpg"}]]
            [:li; {:style "position: absolute; top: 0px; left: -960px; display: none; z-index: 3; opacity: 1; width: 960px; height: 323px;"} 
-            [:img {:src "images/3.jpg"}]]]
+         [:img {:src "images/3.jpg"}]]]
           [:div#slideshow-nav-holder
            [:div#slideshow-nav
             [:a {:href "images/2.jpg" :class "activeSlide"}]]]]
-        [:div.headline "Game"]
-        [:div.shadow-divider]
-        [:div.front-left-col
-         [:div.bullet-title
-          [:div.big "BIG"]
-          [:div.small "SMALL"]]]
+         [:div.headline "Game"]
+         [:div.shadow-divider]
+      [:div.front-left-col
+          [:div.bullet-title
+           [:div.big "BIG"]
+           [:div.small "SMALL"]]
+       left-content]
+      [:div.front-middle-coll]
          [:div.front-right-col
-         [:div.bullet-title
-          [:div.big "BIG right"]
-          [:div.small "SMALL"]]]]]]])))
+          [:div.bullet-title
+           [:div.big "BIG right"]
+           [:div.small "SMALL"]]
+       right-content]]]]])))
+
+
+
+(defn- get-reviews
+  [game-name]
+  (let [game (get-game-by-name game-name)]
+    (for [review (game :critics)]
+        [:div.games-list
+         [:br]
+         [:div.critic-name
+           (review :name)]
+         [:br]
+         [:div.critic-body
+           (review :body)]
+         [:br]
+          [:div.critic-date 
+           [:div.inline
+           (review :date)
+           [:div.critic-game-score (review :score)]]]
+         [:br]])))
+
+(defn get-game-information
+  [game-name]
+  (let [game (get-game-by-name game-name)
+        img (game :picture)]
+    [:ul.blocks-holder
+     [:li.block
+      [:img {:src img :class "thumb"}]]
+     ]))
+
+;(defn game-content
+  ;[game])
 
 ;(def d (parse "index.html"))
 
 (defn home-page1 []
   (layout 
-    [:h1 (str "Hello " (session/flash-get :username))]
-    ;[:h1 (str "Hello " (session/flash-get :name))]
-   [:h1 (link-to "/logout" "Logout")]
-   ))    
+   ; [:h1 (str "Hello " (session/flash-get :username))]
+   ;[:h1 (str "Hello " (session/flash-get :name))]
+  ;[:h1 (link-to "/logout" "Logout")]
+ 
+  (get-game-information "Diablo")
+   (get-reviews "Diablo")
+  
+   )
+  )    
        
 (defroutes home-routes
   (GET "/home" [] (home-page1))
